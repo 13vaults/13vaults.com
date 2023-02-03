7d5b861263e58a1e234b42d1f5b6457b94baf028
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-02-02 23:05:36 -0500

    Add trailing slash config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx"],
+  trailingSlash: true,
 };
 
 module.exports = withContentlayer(nextConfig);
