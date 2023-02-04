0c0628f48a19b648a78448d53d195c6ab598d75f
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-02-04 11:50:06 -0500

    Prefetch some resources

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,11 @@ export default function VaultsApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="prefetch script" href="https://plausible.io/js/script.js" />
+        <link
+          rel="prefetch stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Vollkorn:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Vollkorn+SC:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        />
         <title>The 13 Vaults</title>
       </Head>
       <PlausibleProvider domain="13vaults.com">
