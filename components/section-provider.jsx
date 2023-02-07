2af626c6753a4c5ed2bf5b7558cff63138aea70e
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-02-07 00:46:18 -0500

    Fix weird issue with scrolling highlight

--- components/section-provider.jsx
+++ components/section-provider.jsx
@@ -56,8 +56,7 @@ function useVisibleSections(sectionStore) {
       ) {
         const { id, headingRef, offsetRem } = sections[sectionIndex];
         const offset = remToPx(offsetRem);
-        const top =
-          headingRef.current?.getBoundingClientRect().top ?? offset + scrollY;
+        const top = headingRef.current.getBoundingClientRect().top + scrollY;
 
         if (sectionIndex === 0 && top - offset > scrollY) {
           newVisibleSections.push("_top");
