a3da9e21b3d393e4172f72b218d7e216e71c5ee5
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-02-07 22:05:17 -0500

    Finish up barbarian

--- components/compendium-ability.tsx
+++ components/compendium-ability.tsx
@@ -1,4 +1,20 @@
-import { ReactNode } from "react";
+import { map } from "lodash";
+import { Children, ReactNode } from "react";
+
+function AbilityList({ children }: { children: ReactNode }) {
+  return (
+    <div
+      role="list"
+      className="my-4 list-none m-0 p-0 flex flex-col sm:grid sm:grid-cols-2 2xl:grid-cols-3 grid-rows-[masonry] gap-2"
+    >
+      {map(Children.toArray(children), (child, idx) => (
+        <div role="listitem" key={idx}>
+          {child}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function FeatList({ children }: { children: ReactNode }) {
   return (
@@ -50,16 +66,16 @@ export default function CompendiumAbility({
   return (
     <figure
       role="figure"
-      className="text-xs rounded shadow border border-stone-300 dark:border-stone-700"
+      className="text-xs m-0 rounded shadow border border-stone-300 dark:border-stone-700 bg-stone-50 dark:bg-stone-800"
     >
       <header className="not-prose p-2 gap-2 shadow-sm font-display font-medium text-sm sm:text-base bg-white dark:bg-stone-700 border-b dark:text-stone-200 dark:border-stone-900 border-stone-300 rounded-t flex justify-between">
         <h1 className="text-left">{name}</h1>
         <h2 className="text-right ordinal">{type}</h2>
       </header>
-      <div className="bg-stone-50 dark:bg-stone-800 rounded-b">
+      <div className="rounded-b flex-col">
         <div
           className="p-2 prose max-w-none prose-p:text-justify dark:prose-invert prose-headings:font-display
-                        text-xs first:prose-p:mt-0 last:prose-p:mb-0 prose-hr:my-2 prose-p:my-2 prose-hr:border-stone-300 prose-hr:dark:border-stone-700"
+                     text-xs first:prose-p:mt-0 last:prose-p:mb-0 prose-hr:my-2 prose-p:my-2 prose-hr:border-stone-300 prose-hr:dark:border-stone-700"
         >
           {children}
         </div>
@@ -75,3 +91,4 @@ export default function CompendiumAbility({
 
 CompendiumAbility.FeatList = FeatList;
 CompendiumAbility.Feat = Feat;
+CompendiumAbility.AbilityList = AbilityList;
