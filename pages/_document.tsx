04c249e88306fdf771e81b4bb3526456bd27734f
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-02-23 22:18:46 -0500

    Add locale in html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { defaultLocale } from "@/lib/locales";
+import { get } from "lodash";
+import { Html, Head, Main, NextScript, DocumentProps } from "next/document";
 
-export default function Document() {
+export default function Document(properties: DocumentProps) {
+  const locale = String(
+    get(properties, "__NEXT_DATA__.query.locale", defaultLocale)
+  );
+
   return (
-    <Html lang="en-us">
+    <Html lang={locale}>
       <Head />
       <body className="dark:bg-black">
         <Main />
