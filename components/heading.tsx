8cce4906dcf3ad971c894bf57557ddbb382e8ba5
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-02-25 21:47:13 -0500

    Add new eslint rules

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,20 +1,6 @@
 import clsx from "clsx";
-import { ReactNode, SVGProps, useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import { useSectionStore } from "./section-provider";
-
-function AnchorIcon(properties: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      viewBox="0 0 20 20"
-      fill="none"
-      strokeLinecap="round"
-      aria-hidden="true"
-      {...properties}
-    >
-      <path d="m6.5 11.5-.964-.964a3.535 3.535 0 1 1 5-5l.964.964m2 2 .964.964a3.536 3.536 0 0 1-5 5L8.5 13.5m0-5 3 3" />
-    </svg>
-  );
-}
 
 interface HeadingP {
   level: 1 | 2 | 3 | 4 | 5 | 6;
