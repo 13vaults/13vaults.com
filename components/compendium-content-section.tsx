4c0cf80fee1f00491ddcd4336876d906e12b7419
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-02-26 22:40:56 -0500

    Add border to compendium content sections

--- components/compendium-content-section.tsx
+++ components/compendium-content-section.tsx
@@ -9,7 +9,7 @@ export default function CompendiumContentSection({
 }) {
   return (
     <div className="shadow">
-      <div className="bg-stone-700 dark:bg-stone-800 py-2 px-4 text-xl font-display font-semibold rounded-t text-stone-50">
+      <div className="bg-stone-700 dark:bg-stone-800 py-2 px-4 text-xl font-display font-semibold rounded-t text-stone-50 border-b border-stone-900">
         {header}
       </div>
       <div className="bg-stone-50 dark:bg-stone-900 dark:text-stone-50 p-4 rounded-b gap-4 bg-gradient-to-tl dark:bg-gradient-to-br from-teal-50/25 to-stone-50 dark:from-teal-900/25 dark:to-transparent">
