a383ec544b054128bb56ac1d598ef11d8a252be1
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-03-04 10:01:36 -0500

    Maybe add og image gen?

---
+++ pages/api/og-image.tsx
@@ -0,0 +1,25 @@
+import SocialHero from "@/components/social-hero";
+import { ImageResponse } from "@vercel/og";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler() {
+  const fontData = await fetch(
+    new URL("../../assets/VollkornSC-Bold.TTF", import.meta.url)
+  ).then((response) => response.arrayBuffer());
+
+  return new ImageResponse(<SocialHero fontFamily="Vollkorn SC" />, {
+    width: 800,
+    height: 400,
+    fonts: [
+      {
+        name: "Vollkorn SC",
+        data: fontData,
+        style: "normal",
+        weight: 700,
+      },
+    ],
+  });
+}
