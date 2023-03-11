07493117724e8ecc3d2fa7ef6d18c1cd4feed389
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-03-11 02:07:15 -0500

    Don't restrict to blog post titles

--- pages/api/og-image.png.tsx
+++ pages/api/og-image.png.tsx
@@ -1,4 +1,3 @@
-import { allBlogPosts } from "@/.contentlayer/generated";
 import SocialHero from "@/components/social-hero";
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
@@ -7,33 +6,27 @@ export const config = {
   runtime: "edge",
 };
 
-const allowedTitles = new Set(allBlogPosts.map((post) => post.title));
-
 export default async function handler(request: NextRequest) {
   const { searchParams } = request.nextUrl;
   const title = searchParams.get("title");
 
-  if (title && allowedTitles.has(title)) {
-    const fontData = await fetch(
-      new URL("../../assets/VollkornSC-Bold.ttf", import.meta.url)
-    ).then((response) => response.arrayBuffer());
+  const fontData = await fetch(
+    new URL("../../assets/VollkornSC-Bold.ttf", import.meta.url)
+  ).then((response) => response.arrayBuffer());
 
-    return new ImageResponse(
-      <SocialHero fontFamily="Vollkorn SC" title={title} />,
-      {
-        width: 1200,
-        height: 630,
-        fonts: [
-          {
-            name: "Vollkorn SC",
-            data: fontData,
-            style: "normal",
-            weight: 700,
-          },
-        ],
-      }
-    );
-  } else {
-    return new Response("Invalid title", { status: 400 });
-  }
+  return new ImageResponse(
+    <SocialHero fontFamily="Vollkorn SC" title={title} />,
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Vollkorn SC",
+          data: fontData,
+          style: "normal",
+          weight: 700,
+        },
+      ],
+    }
+  );
 }
