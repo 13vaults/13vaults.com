eac291bfd561863579c52327ee5b19a96d51839f
Sean Clayton 🎷🐢 <me@seanclayton.dev>
2024-05-20 20:06:13 -0400

    Use alert component

--- components/compendium-header.tsx
+++ components/compendium-header.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import CompendiumTitle from "./compendium-title";
+import Alert from "./alert";
 
 export default function VaultHeader({
   primaryLabel,
@@ -16,8 +17,10 @@ export default function VaultHeader({
       <p className="my-0 font-serif font-medium italic text-black/50 dark:text-stone-400">
         {secondaryLabel}
       </p>
-      <div className="mt-4 bg-amber-100 dark:bg-amber-400/25 text-black dark:text-amber-100 rounded-sm py-1 px-2">
-        <p>{t("compendium.edition-warning")}</p>
+      <div className="mt-4">
+        <Alert variant="caution" showIcon={false}>
+          {t("compendium.edition-warning")}
+        </Alert>
       </div>
     </header>
   );
