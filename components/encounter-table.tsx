61d7d9e49cd29444b15e7a9153bdcb48ae25fae2
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2025-06-27 12:29:41 -0400

    Fix typescript issue

--- components/encounter-table.tsx
+++ components/encounter-table.tsx
@@ -1,20 +1,14 @@
-import { toInteger } from "lodash-es";
-import { Trans, useTranslation } from "next-i18next";
-import Link from "next/link";
+import { useTranslation } from "next-i18next";
+import { CSSProperties } from "react";
 
 interface EncounterTableP {
   battleLevel: number;
 }
-export default function EncounterTable({
-  battleLevel = 1,
-}: EncounterTableP) {
+export default function EncounterTable({ battleLevel = 1 }: EncounterTableP) {
   const { t } = useTranslation("calculator");
-  const tableStyle = {
+  const tableStyle: CSSProperties = {
     textAlign: "center",
-  }
-  const thStyle = {
-    textWrap: "auto",
-  }
+  };
 
   return (
     <>
@@ -22,11 +16,11 @@ export default function EncounterTable({
         <table style={tableStyle}>
           <thead>
             <tr>
-              <th style={thStyle}>{t("monsterlevel")}</th>
-              <th style={thStyle}>{t("standard", { mooks: 5 })}</th>
-              <th style={thStyle}>{t("elite", { mooks: '7-8' })}</th>
-              <th style={thStyle}>{t("large", { mooks: 10 })}</th>
-              <th style={thStyle}>{t("huge", { mooks: 15 })}</th>
+              <th>{t("monsterlevel")}</th>
+              <th>{t("standard", { mooks: 5 })}</th>
+              <th>{t("elite", { mooks: "7-8" })}</th>
+              <th>{t("large", { mooks: 10 })}</th>
+              <th>{t("huge", { mooks: 15 })}</th>
             </tr>
           </thead>
           <tbody>
@@ -52,11 +46,21 @@ export default function EncounterTable({
               <td>2</td>
             </tr>
             <tr>
-              <td><strong>{battleLevel}</strong></td>
-              <td><strong>1</strong></td>
-              <td><strong>1.5</strong></td>
-              <td><strong>2</strong></td>
-              <td><strong>3</strong></td>
+              <td>
+                <strong>{battleLevel}</strong>
+              </td>
+              <td>
+                <strong>1</strong>
+              </td>
+              <td>
+                <strong>1.5</strong>
+              </td>
+              <td>
+                <strong>2</strong>
+              </td>
+              <td>
+                <strong>3</strong>
+              </td>
             </tr>
             <tr>
               <td>{battleLevel + 1}</td>
