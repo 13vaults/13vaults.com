532931011e09b147c6dcd7a687b4eb0d36625769
Ben Straub <ben@straub.cc>
2025-06-27 08:26:17 -0700

    Match table to book draft

--- components/encounter-table.tsx
+++ components/encounter-table.tsx
@@ -9,27 +9,40 @@ export default function EncounterTable({
   battleLevel = 1,
 }: EncounterTableP) {
   const { t } = useTranslation("calculator");
+  const tableStyle = {
+    textAlign: "center",
+  }
+  const thStyle = {
+    textWrap: "auto",
+  }
 
   return (
     <>
-      <div className="overflow-auto w-min max-w-full border dark:border-stone-700 rounded shadow bg-white dark:bg-stone-700 border-stone-300 my-2">
-        <table>
+      <div className="overflow-auto max-w-full border dark:border-stone-700 rounded shadow bg-white dark:bg-stone-700 border-stone-300 my-2">
+        <table style={tableStyle}>
           <thead>
             <tr>
-              <th>{t("monsterlevel")}</th>
-              <th>{t("standard", { mooks: 5 })}</th>
-              <th>{t("elite", { mooks: '7-8' })}</th>
-              <th>{t("large", { mooks: 10 })}</th>
-              <th>{t("huge", { mooks: 15 })}</th>
+              <th style={thStyle}>{t("monsterlevel")}</th>
+              <th style={thStyle}>{t("standard", { mooks: 5 })}</th>
+              <th style={thStyle}>{t("elite", { mooks: '7-8' })}</th>
+              <th style={thStyle}>{t("large", { mooks: 10 })}</th>
+              <th style={thStyle}>{t("huge", { mooks: 15 })}</th>
             </tr>
           </thead>
           <tbody>
             <tr>
+              <td>{battleLevel - 3}</td>
+              <td>{battleLevel - 3 >= 0 ? "0.3" : "—"}</td>
+              <td>{battleLevel - 3 >= 0 ? "0.5" : "—"}</td>
+              <td>{battleLevel - 3 >= 0 ? "0.7" : "—"}</td>
+              <td>{battleLevel - 3 >= 0 ? "1" : "—"}</td>
+            </tr>
+            <tr>
               <td>{battleLevel - 2}</td>
-              <td>0.5</td>
-              <td>0.7</td>
-              <td>1</td>
-              <td>1.5</td>
+              <td>{battleLevel - 2 >= 0 ? "0.5" : "—"}</td>
+              <td>{battleLevel - 2 >= 0 ? "0.7" : "—"}</td>
+              <td>{battleLevel - 2 >= 0 ? "1" : "—"}</td>
+              <td>{battleLevel - 2 >= 0 ? "1.5" : "—"}</td>
             </tr>
             <tr>
               <td>{battleLevel - 1}</td>
@@ -39,21 +52,11 @@ export default function EncounterTable({
               <td>2</td>
             </tr>
             <tr>
-              <td>
-                <strong>{battleLevel}</strong>
-              </td>
-              <td>
-                <strong>1</strong>
-              </td>
-              <td>
-                <strong>1.5</strong>
-              </td>
-              <td>
-                <strong>2</strong>
-              </td>
-              <td>
-                <strong>3</strong>
-              </td>
+              <td><strong>{battleLevel}</strong></td>
+              <td><strong>1</strong></td>
+              <td><strong>1.5</strong></td>
+              <td><strong>2</strong></td>
+              <td><strong>3</strong></td>
             </tr>
             <tr>
               <td>{battleLevel + 1}</td>
@@ -65,9 +68,16 @@ export default function EncounterTable({
             <tr>
               <td>{battleLevel + 2}</td>
               <td>2*</td>
-              <td>3**</td>
-              <td>4**</td>
-              <td>6**</td>
+              <td>3*</td>
+              <td>4*</td>
+              <td>6*</td>
+            </tr>
+            <tr>
+              <td>{battleLevel + 3}</td>
+              <td>3*</td>
+              <td>4*</td>
+              <td>6*</td>
+              <td>8*</td>
             </tr>
           </tbody>
         </table>
@@ -75,9 +85,6 @@ export default function EncounterTable({
 
       <p>
         <em>{t("footnote1")}</em>
-      </p>
-      <p>
-        <em>{t("footnote2")}</em>
       </p>
     </>
   );
